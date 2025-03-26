c42b10249a4d549eb20c6a48124ee00767052f96
Soojin6943 <sjhan0711@gmail.com>
2025-03-26 22:59:00 +0900

    mysql 연결 + 기본 폼 작성 + cookie login 구현중

---
+++ src/main/java/org/mySite/user/dto/JoinRequest.java
@@ -0,0 +1,33 @@
+package org.mySite.user.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.mySite.user.domain.User;
+import org.mySite.user.domain.UserRole;
+
+@Getter
+@NoArgsConstructor
+public class JoinRequest {
+
+    @NotBlank(message = "로그인 아이디를 입력하세요.")
+    private String loginId;
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    private String password;
+    @NotBlank
+    private String passwordCheck;
+
+    @NotBlank(message = "닉네임을 입력하세요.")
+    private String nickname;
+
+    public User toEntity() {
+        return User.builder()
+                .loginId(this.loginId)
+                .password(this.password)
+                .nickname(this.nickname)
+                .role(UserRole.USER)
+                .build();
+    }
+}
