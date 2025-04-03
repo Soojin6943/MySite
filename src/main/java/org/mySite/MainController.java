96b6633c9e73688aae9ed74227dc770e99ac540a
Soojin6943 <sjhan0711@gmail.com>
2025-04-04 00:52:51 +0900

    [메인 페이지] 메인페이지 추가(루트 디렉토리)

---
+++ src/main/java/org/mySite/MainController.java
@@ -0,0 +1,13 @@
+package org.mySite;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "main";
+    }
+}
