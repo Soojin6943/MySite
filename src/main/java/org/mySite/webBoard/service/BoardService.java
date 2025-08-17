ecc325a8b8ab66964dea387f53d542ff71995871
Soojin6943 <sjhan0711@gmail.com>
2025-08-18 01:26:12 +0900

    feat: 게시판 리스트 & 상세 페이지

--- src/main/java/org/mySite/webBoard/service/BoardService.java
+++ src/main/java/org/mySite/webBoard/service/BoardService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.mySite.webBoard.domain.Board;
 import org.mySite.webBoard.repository.BoardRepository;
 import org.springframework.stereotype.Service;
+import org.mySite.DataNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,7 +17,20 @@ public class BoardService {
     // todo : impl 만들어서 분리하기
     private final BoardRepository boardRepository;
 
+    // 게시판 리스트
     public List<Board> getList() {
         return this.boardRepository.findAll();
+    }
+
+    // 게시판 상세 페이지
+    public Board getBoard(Long id){
+        // id로 게시판 조회
+        Optional<Board> board = this.boardRepository.findById(id);
+        // optional 객체이므로 if~else로 검사
+        if (board.isPresent()){
+            return board.get();
+        } else { // 없으면 예외처리
+            throw new DataNotFoundException("board not found");
+        }
     }
 }
